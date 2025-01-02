657764fb1c790c3544159f47b2abe4a92e81d2c3
kevinramos2 <keramosl@unal.edu.co>
2025-01-02 14:26:41 -0500

    Update nave.py

--- nave.py
+++ nave.py
@@ -5,3 +5,16 @@ class Nave():
   def __init__(self,pantalla):
     self.pantalla = pantalla
     
+    #Cargar la imagen de la nave
+    self.imagen = pygame.image.load("/img/space-ship-148536_640.bmp")
+    #rectangulo de la imagen "rect"
+    self.rect = self.imagen.get_rect()
+    self.pantallaRect = pantalla.get_rect()
+
+    #Empieza la nave en la parte inferior central de la pantalla
+    self.rect.centerx = self.pantallaRect.centerx
+    self.rect.bottom = self.pantallaRect.bottom
+
+  #Dibuja la nave en la ubicación actual
+  def mostrarNave(self):
+    self.pantalla.blit(self.imagen,self.rect)
