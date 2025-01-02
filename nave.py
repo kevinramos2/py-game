094c4e16c721b34fd0d786493364c82c0f92dcc9
kevinramos2 <keramosl@unal.edu.co>
2025-01-02 14:16:11 -0500

    nave img y clase

---
+++ nave.py
@@ -0,0 +1,7 @@
+import pygame
+#Gestionar el comportamiento de la nave
+class Nave():
+  #inicializa la nave y especificamos el punto de partida
+  def __init__(self,pantalla):
+    self.pantalla = pantalla
+    
